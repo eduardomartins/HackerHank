0c4a505fed6f8796c34016452ecdeb066f102694
Eduardo Sant'Anna Martins <eduardo@eduardomartins.site>
2017-12-22 02:37:28 -0200

    First challenge ready!

---
+++ C#/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace HelloWorld
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine("Hello, World. ");
+            Console.WriteLine(inputString);
+        }
+    }
+}
