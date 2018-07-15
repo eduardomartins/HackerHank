87e625caa18bf518df28197d9dc8f1fb9db7536e
Eduardo Sant' Anna Martins <eduardo@eduardomartins.site>
2018-07-15 12:06:32 -0300

    Problens "A Very Big Sum" and "Diagonal Difference" solved

---
+++ C#/Diagonal Difference/Diagonal Difference/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the diagonalDifference function below.
+    static int diagonalDifference(int[][] arr)
+    {
+        int PmrDim = 0, SndDim = 0, n = arr.Length - 1;
+
+        for (int i = 0;i < arr.Length; i++)
+        {
+            PmrDim += arr[i][i];
+        }
+        
+        for (int i = 0; i < arr.Length; i++)
+        {
+            SndDim += arr[i][n-i];
+        }
+
+        return Math.Abs(PmrDim-SndDim);
+    }
+
+    static void Main(string[] args)
+    {
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[][] arr = new int[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+
+        int result = diagonalDifference(arr);
+
+        Console.WriteLine(result);
+    }
+}
