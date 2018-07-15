69bc6bd169444c1925750d8d91e1a3a5549c2524
Eduardo Sant' Anna Martins <eduardo@eduardomartins.site>
2018-07-15 15:19:55 -0300

    Problem Mini-Max Sum

---
+++ C#/Mini-Max Sum/Mini-Max Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System;
+
+class Solution
+{
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(long[] arr)
+    {
+        long sum = arr.Sum(), max = 0, min = sum , result;
+
+        for (int i = 0; i < 5;i++)
+        {
+            result = sum - arr[i];
+
+            if (result > max)
+            {
+                max = result;
+            }
+
+            if (result < min)
+            {
+                min = result;
+            }
+        }
+
+        Console.Write(min);
+        Console.Write(" ");
+        Console.WriteLine(max);
+        Console.ReadLine();
+    }
+
+    static void Main(string[] args)
+    {
+        long[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt64(arrTemp));
+
+        miniMaxSum(arr);
+    }
+}
