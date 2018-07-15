19778ca291205703e26ac85806d4e23f30e1017f
Eduardo Sant' Anna Martins <eduardo@eduardomartins.site>
2018-07-15 13:47:24 -0300

    Problem Plus Minus solved

---
+++ C#/Plus Minus/Plus Minus/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System;
+
+class Solution
+{
+
+    // Complete the staircase function below.
+    static void staircase(int n)
+    {
+        for(int i = 1;i <= n;i++)
+        {
+            Console.WriteLine(
+                String.Concat(
+                    String.Concat(Enumerable.Repeat(" ", (n-i))),
+                    String.Concat(Enumerable.Repeat("#", i))
+                )
+            );
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        staircase(n);
+    }
+}
